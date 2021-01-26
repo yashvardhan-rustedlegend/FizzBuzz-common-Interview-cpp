d63872374b2353d3e0a98d58ab5e4a0950a86567
Yash Vardhan Tyagi <77291864+yashvardhan-rustedlegend@users.noreply.github.com>
2021-01-26 14:58:25 +0530

    Code

---
+++ Fizz Buzz.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ 	for(int i=1;i<=100;i++)
+ 	{
+ 		if(i%3==0 && i%5==0)
+ 		{
+ 			cout<<"FizzBuzz"<<endl;
+ 			//break;
+		}
+		else if(i%5==0)
+		{
+			cout<<"Buzz"<<endl;
+		}
+		else if(i%3==0)
+		{
+			cout<<"Fizz"<<endl;
+		}
+		else
+		{
+			cout<<i<<endl;
+		}
+	}
+	
+}
